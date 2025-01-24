e9ec194119e1aca1f18929d4edab6d6923f67572
ChristmasLover12345 <madnes0205@gmail.com>
2025-01-23 23:25:09 -0800

    Finished gamePages Ui, started on the backend.

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+
+
+async function helloFetch()
+{
+    const promise = await fetch("https://jherediaall41apis-e2gkayhtajhkdhb6.westus-01.azurewebsites.net/HelloWorld/HelloWorld")
+    const data = await promise.text()
+    console.log(data)
+    return data
+}
+
+helloFetch()
